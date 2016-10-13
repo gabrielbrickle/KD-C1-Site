e63a97791dd2151a22e6267f5eff2960d6386cfd
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-10-13 19:00:41 -0400

    working calc

--- javascripts/app.js
+++ javascripts/app.js
@@ -18,6 +18,73 @@ Tc.Module = Tc.Module.extend({
   }
 });
 
+///new
+/*!
+* Raving Roo - Simple Mortgage Payment Calculator (SMPC) v1.0
+* Copyright 2014 by David K. Sutton
+*
+* You are free to use this script on your website.
+* While not required, it would be much appreciated if you could link back to http://ravingroo.com
+*/
+
+function validNumber(fieldinput){
+var unicode=fieldinput.charCode? fieldinput.charCode : fieldinput.keyCode;
+if ((unicode!=8) && (unicode!=46)) { //if the key isn't the backspace key (which we should allow)
+if (unicode<48||unicode>57) //if not a number
+return false; //disable key press
+}
+}
+
+function myPayment()
+{
+// Reset error messages to blank
+document.getElementById('loanError').innerHTML = '';
+document.getElementById('yearsError').innerHTML = '';
+document.getElementById('rateError').innerHTML = '';
+
+// Form validation checking
+if ((document.mortgagecalc.loan.value === null) || (document.mortgagecalc.loan.value.length === 0) || (isNaN(document.mortgagecalc.loan.value) === true))
+{
+document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
+document.getElementById('loanError').innerHTML = 'Numeric value required. Example: 165000';
+} else if ((document.mortgagecalc.years.value === null) || (document.mortgagecalc.years.value.length === 0) || (isNaN(document.mortgagecalc.years.value) === true))
+{
+document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
+document.getElementById('yearsError').innerHTML = 'Numeric value required. Example: 30';
+} else if ((document.mortgagecalc.rate.value === null) || (document.mortgagecalc.rate.value.length === 0) || (isNaN(document.mortgagecalc.rate.value) === true))
+{
+document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
+document.getElementById('rateError').innerHTML = 'Numeric value required. Example: 5.25';
+} else
+{
+// Set variables from form data
+var loanprincipal = document.mortgagecalc.loan.value;
+var months = document.mortgagecalc.years.value * 12;
+var interest = document.mortgagecalc.rate.value / 1200;
+
+// Calculate mortgage payment and display result
+document.getElementById('monthlyPayment').innerHTML = 'Your monthly mortgage payment will be ' + '$' + (loanprincipal * interest / (1 - (Math.pow(1/(1 + interest), months)))).toFixed(2)+'.';
+document.getElementById('friendlyReminder').style.display = 'block';
+}
+
+// payment = principle * monthly interest/(1 - (1/(1+MonthlyInterest)*Months))
+
+}
+
+function myPaymentReset()
+{
+// Reset everything to default/null/blank
+document.getElementById('monthlyPayment').innerHTML = 'Values reset';
+document.getElementById('friendlyReminder').style.display = 'none';
+document.getElementById('loanError').innerHTML = '';
+document.getElementById('yearsError').innerHTML = '';
+document.getElementById('rateError').innerHTML = '';
+document.mortgagecalc.loan.value = null;
+document.mortgagecalc.years.value = null;
+document.mortgagecalc.rate.value = null;
+}
+///end
+
 jQuery.extend({
   randomColor: function() {
     return '#' + Math.floor(Math.random()*256*256*256).toString(16);
@@ -764,10 +831,10 @@ jQuery(document).foundation();
   })
 })(Tc.$);
 (function($) {
-  Tc.Module.SectionHeader = Tc.Module.extend({    
+  Tc.Module.SectionHeader = Tc.Module.extend({
     init: function($ctx, sandbox, modId) {
       this._super($ctx, sandbox, modId);
-    },    
+    },
     dependencies: function() {
     },
     onBinding: function() {
@@ -821,9 +888,9 @@ jQuery(document).foundation();
 
       var path = window.location.pathname;
 
-      
+
         if ( path.match(/^\/wrap(\/)?(\/\w+\.html)?$|(\/demo\-[2-3])/) ) {
-      
+
 
         var color_scheme = this.readCookie('color_scheme');
 
@@ -885,13 +952,13 @@ jQuery(document).foundation();
         if (target) {
           var host = window.location.host;
           if ( target == 'demo-1' ) {
-            
+
               window.location = 'http://' + host + '/wrap/' + 'index.html';
-            
+
           } else {
-            
+
               window.location = 'http://' + host + '/wrap/' + target + '/index.html';
-            
+
           }
 
         }
@@ -943,5 +1010,3 @@ jQuery(document).foundation();
     }
   })
 })(Tc.$);
-
-
